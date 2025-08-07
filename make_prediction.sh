217ac4557984bb0a5cabc6ea95a282748532864a
Reem  Alzahrani <110042258+Reem8534@users.noreply.github.com>
2025-08-08 01:29:02 +0300

    Update make_prediction.sh

--- make_prediction.sh
+++ make_prediction.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
 
 PORT=5000
-echo "Port: $PORT"
+HOST="10.0.3.78"
+echo "Sending request to http://$HOST:$PORT/predict"
 
-# POST method predict
 curl -d '{  
-   "CHAS":{  
-      "0":0
-   },
-   "RM":{  
-      "0":6.575
-   },
-   "TAX":{  
-      "0":296.0
-   },
-   "PTRATIO":{  
-      "0":15.3
-   },
-   "B":{  
-      "0":396.9
-   },
-   "LSTAT":{  
-      "0":4.98
-   }
-}'\
-     -H "Content-Type: application/json" \
-     -X POST http://localhost:$PORT/predict
+   "CHAS":{ "0":0 },
+   "RM":{ "0":6.575 },
+   "TAX":{ "0":296.0 },
+   "PTRATIO":{ "0":15.3 },
+   "B":{ "0":396.9 },
+   "LSTAT":{ "0":4.98 }
+}' \
+  -H "Content-Type: application/json" \
+  -X POST http://$HOST:$PORT/predict
